84ff26620c0a27f1149d496604f4ce0969a34f3e
= <=>
2013-02-02 16:08:49 -0700

    Added IFactory in order to decouple higher-order frameworks. See new AutoImplementedAttribute in FlitBit.Emit for more info.

---
+++ FlitBit.Core/Factory/IFactory.cs
@@ -0,0 +1,21 @@
+﻿
+namespace FlitBit.Core.Factory
+{
+	/// <summary>
+	/// Interface for classes that construct other classes. 
+	/// </summary>
+	/// <remarks>
+	/// This interface is used by the frameworks to decouple components from the underlying IoC container.
+	/// While the FlitBit.IoC's IContainer is-a IFactory, it is a trivial matter to build adapters to
+	/// other IoC containers by implementing IFactory to delegate to the IoC of your choice.
+	/// </remarks>
+	public interface IFactory
+	{
+		/// <summary>
+		/// Creates a new instance of type T.
+		/// </summary>
+		/// <typeparam name="T">type T</typeparam>
+		/// <returns>a new instance</returns>
+		T CreateInstance<T>();
+	}
+}
---
+++ FlitBit.Core/Meta/InstanceScopeKind.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FlitBit.Core.Meta
+{
+	/// <summary>
+	/// Basic enumeration of instance scopes.
+	/// </summary>
+	public enum InstanceScopeKind
+	{
+		/// <summary>
+		/// Indicates that an instance has a natural scope.
+		/// </summary>
+		OnDemand = 0,
+		/// <summary>
+		/// Indicates that an instance has container scope.
+		/// </summary>
+		ContainerScope = 1,
+		/// <summary>
+		/// Indicates that an instance is a singleton.
+		/// </summary>
+		Singleton = 2
+	}
+}
