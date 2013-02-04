56d32969116d4d977d57ffe5e98cbfb6eb29d38c
= <=>
2013-02-04 09:42:29 -0700

    Added contract class for interface IFactory.

--- FlitBit.Core/Factory/IFactory.cs
+++ FlitBit.Core/Factory/IFactory.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System.Diagnostics.Contracts;
+
 namespace FlitBit.Core.Factory
 {
 	/// <summary>
@@ -9,6 +10,7 @@ namespace FlitBit.Core.Factory
 	/// While the FlitBit.IoC's IContainer is-a IFactory, it is a trivial matter to build adapters to
 	/// other IoC containers by implementing IFactory to delegate to the IoC of your choice.
 	/// </remarks>
+	[ContractClass(typeof(CodeContracts.ContractForFactory))]		
 	public interface IFactory
 	{
 		/// <summary>
@@ -18,4 +20,26 @@ namespace FlitBit.Core.Factory
 		/// <returns>a new instance</returns>
 		T CreateInstance<T>();
 	}
+
+	namespace CodeContracts
+	{
+		/// <summary>
+		/// CodeContracts Class for IFactory
+		/// </summary>
+		[ContractClassFor(typeof(IFactory))]
+		public abstract class ContractForFactory : IFactory
+		{
+			/// <summary>
+			/// Creates a new instance of type T.
+			/// </summary>
+			/// <typeparam name="T">type T</typeparam>
+			/// <returns>a new instance</returns>
+			public T CreateInstance<T>()
+			{
+				Contract.Ensures(Contract.Result<T>() != null);
+
+				throw new System.NotImplementedException();
+			}
+		}
+	}		
 }
