7d81d7410dba3d93c92ad6d2fad37cde677c2970
cerebralkungfu <phillip@flitbit.com>
2013-05-23 09:03:00 -0600

    Added LanguageCode class.

---
+++ FlitBit.Core/i18n/LanguageCode.cs
@@ -0,0 +1,25 @@
+﻿
+namespace FlitBit.Core.i18n
+{
+	/// <summary>
+	/// IETF Langage Code.
+	/// </summary>
+	public sealed class LanguageCode
+	{
+		/// <summary>
+		/// The two digit language code.
+		/// </summary>
+		public string Code { get; private set; }
+
+		/// <summary>
+		/// The language's name.
+		/// </summary>
+		public string Language { get; private set; }
+
+		internal LanguageCode(string code, string name)
+		{
+			this.Code = code;
+			this.Language = name;
+		}
+	}
+}
